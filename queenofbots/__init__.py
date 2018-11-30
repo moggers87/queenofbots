62d139f8849e6063138a70893036ea9bff8b1f9f
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2018-11-30 23:53:28 +0000

    Initial commit

---
+++ queenofbots/__init__.py
@@ -0,0 +1,37 @@
+##
+#    Copyright (C) 2018 Matt Molyneaux
+#
+#    This file is part of queenofbots.
+#
+#    Inboxen is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Inboxen is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with Inboxen  If not, see <http://www.gnu.org/licenses/>.
+##
+
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+    )
+
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_pyfile(test_config)
+
+    @app.route("/")
+    def main():
+        return "Hello"
+
+    return app
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import versioneer
+
+
+with open("README.rst", "r") as f:
+    long_desc = f.read()
+
+setup(
+    name="queenofbots",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Matt Molyneaux",
+    author_email="moggers87+git@moggers87.co.uk",
+    description="Mastodon ebooks bot manager",
+    long_description=long_desc,
+    url="https://github.com/moggers87/queenofbots",
+    download_url="https://pypi.org/project/queenofbots/",
+    packages=find_packages(),
+    test_suite="exhibition.tests",
+    license="AGPLv3",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet",
+    ],
+    install_requires=[
+        "flask"
+    ],
+    extras_require={
+        "docs": [
+            "sphinx",
+            "sphinx_rtd_theme",
+            "sphinx-click",
+        ],
+    },
+)
